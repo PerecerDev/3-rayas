25b701eb63552095f970eb6296ac34ad7aa17c95
PerecerDev <perecer.dev@gmail.com>
2024-10-05 16:35:44 +0200

    home title and start button styled both

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
+import Header from "./components/Header"
+import StartButton from "./components/StartButton"
+
 function App() {
 
   return (
     <>
-      <article className="flex w-full h-screen items-center justify-center bg-blue-950">
-        <header className="border text-blue-950 hover:text-blue-900 border-blue-700 rounded-xl px-20 py-14 bg-gradient-to-br from-blue-200 to-blue-300 hover:from-blue-100 hover:to-blue-300 shadow-[0px_0px_100px_0px] shadow-blue-600 hover:shadow-blue-500">
-          <h1 className="text-4xl font-black uppercase flex flex-col gap-2 text-center">
-            <span className="text-9xl w-full text-blue-500">3</span>
-            en Raya
-          </h1>
-        </header>
-        <section>
+      <article className="flex flex-col gap-44 w-full h-screen items-center justify-center bg-blue-950">
+        
+        <Header />
 
+        <section>
+          <StartButton />
         </section>
+
         <footer>
 
         </footer>
+
       </article>
     </>
   )
